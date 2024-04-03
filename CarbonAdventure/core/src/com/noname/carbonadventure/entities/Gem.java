86752f14f2db13cc29c89a75472bb61b033ea21f
Darragh Redmond <darragh.redmond@ucdconnect.ie>
2024-04-03 12:56:02 +0100

    Updated entity and screen classes for gem movement and camera functionality

--- CarbonAdventure/core/src/com/noname/carbonadventure/entities/Gem.java
+++ CarbonAdventure/core/src/com/noname/carbonadventure/entities/Gem.java
@@ -1,2 +1,34 @@
-package com.noname.carbonadventure.entities;public class Gem {
+package com.noname.carbonadventure.entities;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Gem {
+    private Vector2 position;
+    private Texture texture;
+    private boolean collected = false;
+
+    public Gem(Texture texture, Vector2 position) {
+        this.texture = texture;
+        this.position = position;
+    }
+
+    public void draw(SpriteBatch batch) {
+        if (!collected) {
+            batch.draw(texture, position.x, position.y);
+        }
+    }
+
+    public void collect() {
+        collected = true;
+    }
+
+    public boolean isCollected() {
+        return collected;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
 }
+
