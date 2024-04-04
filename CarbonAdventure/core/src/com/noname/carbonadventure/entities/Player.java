0603a9c53c921600ddda56806a9789d84f0c56bc
DR7439 <146729680+DR7439@users.noreply.github.com>
2024-04-04 16:28:55 +0100

    added animation to movement

--- CarbonAdventure/core/src/com/noname/carbonadventure/entities/Player.java
+++ CarbonAdventure/core/src/com/noname/carbonadventure/entities/Player.java
@@ -2,42 +2,82 @@ package com.noname.carbonadventure.entities;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.noname.carbonadventure.screens.AnimationManager;
+import com.badlogic.gdx.graphics.Texture;
 
-public class Player extends Sprite {
-    private Vector2 velocity = new Vector2();
+public class Player {
+    public Vector2 position = new Vector2();
+    public Vector2 velocity = new Vector2();
     private float speed = 60;
+    private PlayerState currentState;
+    private float stateTime = 0;
+    private AnimationManager animationManager;
 
-    public Player(Sprite sprite) {
-        super(sprite);
+    private Texture texture;
+
+    public Player(Vector2 startPosition, AnimationManager animationManager) {
+        this.position.set(startPosition);
+        this.currentState = PlayerState.IDLE;
+        this.animationManager = animationManager;
     }
 
-    @Override
-    public void draw(Batch batch) {
-        update(Gdx.graphics.getDeltaTime());
-        super.draw(batch);
+    public float getX() {
+        return position.x;
+    }
+
+    public float getY() {
+        return position.y;
+    }
+
+    public float getWidth() {
+        return 0;
+    }
+
+    public float getHeight() {
+        return 0;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public void dispose() {
+        texture.dispose();
     }
 
     public void update(float delta) {
-        velocity.x = 0;
-        velocity.y = 0;
+        handleInput();
+        position.add(velocity.x * delta, velocity.y * delta);
+        stateTime += delta;
+        velocity.set(0, 0);
+    }
 
+    private void handleInput() {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             velocity.x = -speed;
+            currentState = PlayerState.WALKING_LEFT;
         } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             velocity.x = speed;
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            currentState = PlayerState.WALKING_RIGHT;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
             velocity.y = speed;
+            currentState = PlayerState.WALKING_UP;
         } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
             velocity.y = -speed;
+            currentState = PlayerState.WALKING_DOWN;
+        } else {
+            currentState = PlayerState.IDLE;
         }
+    }
 
-        setX(getX() + velocity.x * delta);
-        setY(getY() + velocity.y * delta);
+    public void draw(SpriteBatch batch) {
+
+        TextureRegion currentFrame = animationManager.getFrame(currentState, stateTime);
+        batch.draw(currentFrame, position.x, position.y);
     }
 }
 
