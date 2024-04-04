fa22bb8f007dfb3dfb177c9c2ea24cffd235a154
Nathan Power <nathanpower00@yahoo.com>
2024-04-04 12:18:52 +0100

    Updated Movement, Gem, Map

--- CarbonAdventure/core/src/com/noname/carbonadventure/screens/Play.java
+++ CarbonAdventure/core/src/com/noname/carbonadventure/screens/Play.java
@@ -6,10 +6,13 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.noname.carbonadventure.entities.Player;
+import com.noname.carbonadventure.entities.Gem;
+import com.badlogic.gdx.math.Vector2;
 
 public class Play implements Screen {
 
@@ -17,21 +20,28 @@ public class Play implements Screen {
     private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
 
+    private SpriteBatch spriteBatch;
     private Player player;
+    private Texture gemTexture;
+    private Gem gem;
+
 
     @Override
     public void show() {
         TmxMapLoader loader = new TmxMapLoader();
-        map = loader.load("maps/Level_1.tmx");
+        map = loader.load("maps/Level_1.1.tmx");
 
         renderer = new OrthogonalTiledMapRenderer(map);
 
         camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
+        Vector2 startPosition = new Vector2(340, 340);
         player = new Player(new Sprite(new Texture("img/player.png")));
+        player.setPosition(startPosition.x, startPosition.y);
 
-
-
+        gemTexture = new Texture(Gdx.files.internal("img/gem.png"));
+        gem = new Gem(gemTexture, new Vector2(-10, -10));
     }
 
     @Override
@@ -39,24 +49,34 @@ public class Play implements Screen {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Update game logic here (e.g., player movement)
+        player.update(delta);
+
+        // Make the camera follow the player
+        camera.position.set(player.getX() + player.getWidth() / 2, player.getY() + player.getHeight() / 2, 0);
+        camera.update();
+
+        renderer.setView(camera);
+        renderer.render();
+
+
         renderer.setView(camera);
         renderer.render();
 
         renderer.getBatch().begin();
         player.draw(renderer.getBatch());
         renderer.getBatch().end();
-
-
-
     }
+
 
     @Override
     public void resize(int width, int height) {
         camera.viewportWidth = width;
         camera.viewportHeight = height;
         camera.update();
-
-
     }
 
     @Override
@@ -80,6 +100,7 @@ public class Play implements Screen {
         map.dispose();
         renderer.dispose();
         player.getTexture().dispose();
+        gemTexture.dispose();
 
     }
 }
