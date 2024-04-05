ea639eeb34df6d32b9701bdaf677ee106b1fc56e
Nathan Power <90730341+nathanpower1@users.noreply.github.com>
2024-04-06 00:41:00 +0100

    Add files via upload

---
+++ NewMethodGame/core/src/com/noname/carbonadventure/Sprites/Gem.java
@@ -0,0 +1,17 @@
+package com.noname.carbonadventure.Sprites;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.noname.carbonadventure.Play;
+
+public class Gem extends InteractiveTileObject{
+    public Gem(World world, TiledMap map, Rectangle bounds){
+        super(world,map,bounds);
+
+
+    }
+}
