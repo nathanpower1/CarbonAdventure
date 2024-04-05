8ee9661030d27a69fdbd57ecdee4bc7c4f2e094e
Nathan Power <90730341+nathanpower1@users.noreply.github.com>
2024-04-06 00:18:32 +0100

    New_Method
    followed new method and started from scratch

--- CarbonAdventure/desktop/src/com/noname/carbonadventure/DesktopLauncher.java
+++ CarbonAdventure/desktop/src/com/noname/carbonadventure/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.noname.carbonadventure;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.noname.carbonadventure.CarbonAdventure;
+import com.noname.carbonadventure.Play;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +10,6 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("CarbonAdventure");
-		new Lwjgl3Application(new CarbonAdventure(), config);
+		new Lwjgl3Application(new Play(), config);
 	}
 }
