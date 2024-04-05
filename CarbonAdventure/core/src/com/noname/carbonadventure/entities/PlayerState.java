e049cb17e3c56047232c0497eec8a4e39fa55d9d
Nathan Power <90730341+nathanpower1@users.noreply.github.com>
2024-04-06 00:32:00 +0100

    Delete CarbonAdventure directory

--- CarbonAdventure/core/src/com/noname/carbonadventure/entities/PlayerState.java
+++
@@ -1,9 +0,0 @@
-package com.noname.carbonadventure.entities;
-
-public enum PlayerState {
-    IDLE,
-    WALKING_LEFT,
-    WALKING_RIGHT,
-    WALKING_UP,
-    WALKING_DOWN
-}
--- CarbonAdventure/desktop/build/resources/main/maps/Gems.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Gems" tilewidth="16" tileheight="16" tilecount="49" columns="7">
- <image source="gems.png" width="112" height="112"/>
-</tileset>
