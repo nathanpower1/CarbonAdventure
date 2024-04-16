bb60557636b48673b1fbc7e82b1394257a70a720
Niallmckayy <niallmckay71@gmail.com>
2024-04-16 19:14:11 +0100

    Adding functionality to reset gems

--- NewMethodGame/core/src/com/noname/carbonadventure/Sprites/Finish2.java
+++ NewMethodGame/core/src/com/noname/carbonadventure/Sprites/Finish2.java
@@ -3,6 +3,7 @@ package com.noname.carbonadventure.Sprites;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Rectangle;
 import com.noname.carbonadventure.Play;
+import com.noname.carbonadventure.Scenes.HUD;
 import com.noname.carbonadventure.Screens.PlayScreen;
 
 import static com.noname.carbonadventure.Play.player;
@@ -19,7 +20,7 @@ public class Finish2 extends InteractiveTileObject {
 
     @Override
     public void OnBodyHit() {
-
+        HUD.resetGemIcons();
         Gdx.app.log("Finish", "Finish line has been triggered.");
         // 2730/Play.PPM,3180/Play.PPM)
         float destinationX = 2730/Play.PPM;// Change this to the desired X coordinate
