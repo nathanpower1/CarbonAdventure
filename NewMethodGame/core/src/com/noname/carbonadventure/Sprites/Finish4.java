daad5825fea83b6e8721e339ac58029b2afbb1fd
Niallmckayy <niallmckay71@gmail.com>
2024-04-17 17:07:39 +0100

    Adding final score

--- NewMethodGame/core/src/com/noname/carbonadventure/Sprites/Finish4.java
+++ NewMethodGame/core/src/com/noname/carbonadventure/Sprites/Finish4.java
@@ -3,6 +3,7 @@ package com.noname.carbonadventure.Sprites;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Rectangle;
 import com.noname.carbonadventure.Play;
+import com.noname.carbonadventure.Scenes.HUD;
 import com.noname.carbonadventure.Screens.PlayScreen;
 
 import static com.noname.carbonadventure.Play.player;
@@ -19,6 +20,8 @@ public class Finish4 extends InteractiveTileObject {
 
     @Override
     public void OnBodyHit() {
+        int finalscore = HUD.getScore();
+        System.out.println(finalscore);
 
         Gdx.app.log("Finish", "Finish line has been triggered.");
         // 490/Play.PPM,3180/Play.PPM
